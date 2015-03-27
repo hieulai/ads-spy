8f62725a8b7e7bbe8022202d301ef2649bdb097b
Hieu Lai Trung <hieulaitrung@gmail.com>
2015-03-27 11:47:51 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AdsSpy::Application.config.secret_key_base = 'b8f5f06b882ed42e0804f37bdfecaec5fc430303874d17e20e182e0a2daf30c3de5a2c953ea50b308ae1c2418c882784fcbb80914ddaef07f984e305870d4ab9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AdsSpy::Application.config.session_store :cookie_store, key: '_ads-spy_session'
